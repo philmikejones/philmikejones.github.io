b5e766490e6055450545501df2589b346d3c2d7e
Phil Mike Jones <philmikejones@gmail.com>
2021-03-16 21:01:37 +0000

    remove previous/next buttons

--- conf.py
+++ conf.py
@@ -50,6 +50,11 @@ exclude_patterns = [
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'analytics_id': ' UA-44027458-2',
+    'prev_next_buttons_location': 'none'
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
